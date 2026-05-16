850cc377bd664358c5c7bce30dd74e5ede589b7e
fdenarde <fdenarde@users.noreply.github.com>
2026-05-16 12:23:58 -0300

    Auto-deploy: manual change detected

--- gestão-clínica-fábio-denarde/src/components/PersonalAgenda.tsx
+++ gestão-clínica-fábio-denarde/src/components/PersonalAgenda.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { AppState, PersonalAppointment, PersonalAppointmentType, AlarmAdvance, AlarmSound } from '../types';
 import { ChevronLeft, ChevronRight, Calendar as CalendarIcon, List, LayoutGrid, FastForward, Clock, Bell, CheckCircle2, MoreVertical, Plus, Edit2, Trash2, CalendarClock, BookOpen } from 'lucide-react';
-import { format, addDays, startOfWeek, addWeeks, subWeeks, getDay, isSameDay, startOfMonth, endOfMonth, eachDayOfInterval, isBefore, parseISO, isSameMonth, addMonths, subMonths } from 'date-fns';
+import { format, addDays, subDays, startOfWeek, addWeeks, subWeeks, getDay, isSameDay, startOfMonth, endOfMonth, eachDayOfInterval, isBefore, parseISO, isSameMonth, addMonths, subMonths } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Modal from './Common/Modal';
 import { showToast } from './Common/Toast';
